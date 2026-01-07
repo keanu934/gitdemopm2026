e0d85967ea17ee966d53042b32e1d66b6a015f89
keanu934 <keanucrocker@gmail.com>
2026-01-07 12:49:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Git/Github Demo")
-print("Hello from the local repo")
-print("Hello from the remote repo")
+print("Hello from the local repo 2")
+print("Hello from the remote repo 3")
+print("Hey local, you ain't so tough")
